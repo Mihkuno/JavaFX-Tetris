717ee60d948a801adbb53d5b781273f3b4191fab
XaxoriJoey <caindayjoeninyo@gmail.com>
2021-08-05 00:26:07 +0800

    added start_swipe speed func

--- style/Animate.java
+++ style/Animate.java
@@ -115,6 +115,44 @@ public class Animate {
         return bounce;
     }
 
+    public static TranslateTransition start_swipe(Node node, boolean direction, int speed) {
+
+        int init = direction ? -20 : 20;
+        double z = direction ? 500 : -500;
+        double current = 0;
+
+        swipe = new TranslateTransition();  
+        swipe.setDelay(Duration.millis(10));
+        swipe.setDuration(Duration.millis(400+speed)); 
+        swipe.setCycleCount(1);  
+
+        if (node.getTranslateY() == 0) {
+            swipe.setFromY(z);
+            current = init;
+            swipe.setToY(current);
+        }
+        else {
+            swipe.setFromY(node.getTranslateY());
+            current = (node.getTranslateY() - z) + init;
+            swipe.setToY(current);
+        }
+        
+        swipe.setNode(node);
+        swipe.play();
+
+        bounce = new TranslateTransition();  
+        bounce.setDelay(Duration.millis(410+speed));
+        bounce.setDuration(Duration.millis(300+speed)); 
+        bounce.setCycleCount(1);  
+        bounce.setFromY(current);
+        bounce.setToY(current-init);
+        bounce.setNode(node);
+        bounce.play();
+
+        
+        return bounce;
+    }
+
     public static TranslateTransition end_swipe(Node node, boolean direction) {
 
         int init = direction ? -20 : 20;
